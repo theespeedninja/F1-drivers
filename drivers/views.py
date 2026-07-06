45abb2cf1a55eee0b700feeca796c62340e8e0da
theespeedninja <dannygitonga1@gmail.com>
2026-07-06 20:49:14 +0300

    now handled urls and templates

--- drivers/views.py
+++ drivers/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from  django.template import loader
+
 # Create your views here.
 def simple_view(request):
-    return HttpResponse("Hello world!")
+    template = loader.get_template("myfirst.html")
+    return HttpResponse(template.render())
